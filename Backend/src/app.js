989195c1e07d049439ccffc4480b112a978a0987
Kanav <kanavb86@gmail.com>
2026-08-21 23:56:04 +0530

    Update project files

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
 const authRouter = require('./routes/auth.routes')
 const app=express()
 
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use(express.json())
 app.use(cookieParser())
+
 
 app.use('/api/auth',authRouter) // calling auth router api here
 
--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baaseURL:"http://localhost:3000",
+    baseURL:"http://localhost:3000",
     withCredentials:true
 })
 
@@ -18,7 +18,7 @@ export async function register({username,email,password}){
 
 export async function login({email,password}){
     try{
-        const response = await axios.post("/api/auth/login",{
+        const response = await api.post("/api/auth/login",{
             email,password
         })
         return response.data
@@ -29,7 +29,7 @@ export async function login({email,password}){
 
 export async function logout(){
     try{
-        const response = await axios.get("/api/auth/logout")
+        const response = await api.get("/api/auth/logout")
         return response.data
     } catch(err){
         console.log(err)
@@ -38,7 +38,7 @@ export async function logout(){
 
 export async function getMe(){
     try{
-        const response = await axios.get("/api/auth/get-me")
+        const response = await api.get("/api/auth/get-me")
         return response.data
     } catch(err){
         console.log(err)
